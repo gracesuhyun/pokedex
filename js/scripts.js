ec54d88ef03de7efda4fb277b2421cc173a4bfbc
gracesuhyun <gracesuhyun@gmail.com>
2022-04-04 17:02:31 -0400

    created array containing data for 5 Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert('Hello world');
-let favoriteFood = 'soup';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'Butterfree',
+        height: 1.1,
+        types: ['bug', 'flying']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        types: ['electric']
+    },
+    {
+        name: 'Jigglypuff',
+        height: 0.5,
+        types: ['fairy', 'normal']
+    }
+];
